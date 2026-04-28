897d3a09c21e8ffc9e1a852e6a983075a68eba4c
Henrique <henriqueffabro@gmail.com>
2026-04-27 21:27:21 -0300

    Pesquisa funcionando com nome de usuarios

--- sistema.php
+++ sistema.php
@@ -199,7 +199,7 @@ $result = $conexao->query($sql); //executa a consulta SQL e salva o resultado na
 
                     $sql = "SELECT postagens.*, usuarios.nome, usuarios.foto_perfil FROM postagens 
                             JOIN usuarios ON postagens.id_usuario = usuarios.id 
-                            WHERE postagens.titulo LIKE '%$pesquisa%' OR postagens.descricao LIKE '%$pesquisa%'"; //consulta SQL para buscar postagens que tenham o termo de pesquisa no título ou na descrição, e também traz o nome e a foto de perfil do usuário que fez a postagem
+                            WHERE postagens.titulo LIKE '%$pesquisa%' OR postagens.descricao LIKE '%$pesquisa%' OR usuarios.nome LIKE '%$pesquisa%'"; //consulta SQL para buscar postagens que tenham o termo de pesquisa no título ou na descrição, e também traz o nome e a foto de perfil do usuário que fez a postagem
                     $posts = $conexao->query($sql);
                 }
                 else {
